bfdd458ed93f22911b586d8bdec443a8e5a93f77
Dilli_Rao <dilliwebdev218@gmail.com>
2024-03-18 23:53:33 +0530

    Code Refactoring and Logo change
    Removed Focus in root element and added only for specific element and added smooth scroll behavior and changed the Logo and removed scroll JavaScript code

--- javascript/script.js
+++ javascript/script.js
@@ -3,6 +3,7 @@ yearEl.textContent = new Date().getFullYear();
 
 const headerEl = document.querySelector("header");
 const xbtn = document.querySelector(".close-button");
+
 xbtn.addEventListener("click", () => {
   // add will add the class, whereas toggle is used for both on and off
   headerEl.classList.remove("nav-open");
@@ -12,45 +13,46 @@ const navButton = document.querySelector(".mobile-menu-icon");
 navButton.addEventListener("click", () => {
   // add will add the class, whereas toggle is used for both on and off
   headerEl.classList.toggle("nav-open");
+  console.log("clicked");
 });
 
 // //////////////////////////////////////////////////////
 // Smooth Scrolling Animation for safari
+// //////////////////////////////////////////////////////
 
 // gets all the anchor tag links in the page
 const allLinks = document.querySelectorAll("a:link");
 
 // now to access each links from all links , we use for each function
 allLinks.forEach(function (link) {
-  // to access the link that we clicked , adding event listener
+  //   // to access the link that we clicked , adding event listener
   link.addEventListener("click", function (e) {
-    // to prevent deauflt scrolling
-    e.preventDefault();
-
-    // to get each #href attirbute value of the link
+    //     // to prevent deauflt scrolling
+    //     e.preventDefault();
+    // console.log(link);
+    //     // to get each #href attirbute value of the link
     const href = link.getAttribute("href");
-
-    // if  href = # , that is logos  , if clicked on it below function will take it smoothly to top 0
-    if (href === "#") {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-    }
-
-    // // if href is not only # and starts with # and contains a value then this condition will aplly
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-
-      // scrolls into the section
-      sectionEl.scrollIntoView({
-        behavior: "smooth",
-      });
-    }
-
+    console.log(href);
     if (link.classList.contains("main-nav-links")) {
       headerEl.classList.toggle("nav-open");
     }
+    //     // if  href = # , that is logos  , if clicked on it below function will take it smoothly to top 0
+    //     if (href === "#") {
+    //       window.scrollTo({
+    //         top: 0,
+    //         behavior: "smooth",
+    //       });
+    //     }
+
+    //     // // // if href is not only # and starts with # and contains a value then this condition will aplly
+    //     if (href !== "#" && href.startsWith("#")) {
+    //       const sectionEl = document.querySelector(href);
+
+    //       // scrolls into the section
+    //       sectionEl.scrollIntoView({
+    //         behavior: "smooth",
+    //       });
+    //     }
   });
 });
 
